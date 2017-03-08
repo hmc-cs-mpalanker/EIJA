abfaa7e2713097c059301ad894e61276e6430074
Lee Norgaard <leemnorgaard@gmail.com>
2017-03-08 15:22:29 -0800

    added rexml stuff in plays controller rb

--- script_editor/app/controllers/plays_controller.rb
+++ script_editor/app/controllers/plays_controller.rb
@@ -1,6 +1,10 @@
+require "rexml/document"
+include REXML # so that we don't have to prefix everything with REXML::...
+
 class PlaysController < ApplicationController
   def show
-  	render template: "plays/#{params[:play]}"
+  	#render template: "plays/#{params[:play]}"
+  	file = File.new( "FolgerDigitalTexts_XML_Complete/MND.xml" )
+	doc = Document.new file
   end
-
 end
