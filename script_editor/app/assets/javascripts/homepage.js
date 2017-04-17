bd28378342aeb190a1a847a6806362f2fef2b659
Lee Norgaard <leemnorgaard@gmail.com>
2017-04-17 00:05:38 -0700

    fixed numbering of scenes

--- script_editor/app/assets/javascripts/homepage.js
+++ script_editor/app/assets/javascripts/homepage.js
@@ -1,9 +1,9 @@
 // Homepage js
 
 var a = document.getElementById('searchbar');
-    a.addEventListener('submit',function(e) {
-        e.preventDefault();
-        var b = document.getElementById('searchinput').value;
-        document.getElementById("searchquery").innerHTML = b;
-        window.location.href = '/plays/' + b;
-    });
+a.addEventListener('submit',function(e) {
+    e.preventDefault();
+    var b = document.getElementById('searchinput').value;
+    document.getElementById("searchquery").innerHTML = b;
+    window.location.href = '/plays/' + b;
+});
