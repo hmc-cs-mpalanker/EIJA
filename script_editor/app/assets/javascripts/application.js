168a89fec064c389afd94d225ffcf2b5aa96acee
cmxiang <cmxiang88@gmail.com>
2017-05-05 17:17:44 -0700

    Update application.js

--- script_editor/app/assets/javascripts/application.js
+++ script_editor/app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require tinymce-jquery
 //= require jquery-1.4.2.min
 //= require jquery
 //= require jquery_ujs
