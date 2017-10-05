f4f5228688a78115ed5a1f7dad149b6d43c74e00
hmc-cs-mpalanker <mpalanker@cs.hmc.edu>
2017-10-05 15:41:45 -0700

    Allow Multiple Plays at once!

--- script_editor/db/seeds.rb
+++ script_editor/db/seeds.rb
@@ -5,45 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+files = ["FolgerDigitalTexts_XML_Complete/MM.xml", "FolgerDigitalTexts_XML_Complete/MND.xml"]
+files.each do |file|
+  doc = Nokogiri::XML(File.open(file))
+  title = doc.css('title').first
+  play = Play.create(title: title.inner_text)
 
-doc = Nokogiri::XML(File.open("FolgerDigitalTexts_XML_Complete/MND.xml"))
-title = doc.css('title').first
-play = Play.create(title: title.inner_text)
-
-currAct = 1
-currScene = 1
-currIndex = 1
-acts= doc.css('//div1')
-acts.each do |act|
-  newact = play.acts.create(number: currAct)
-  currAct = currAct + 1
+  currAct = 1
   currScene = 1
-  scenes = Nokogiri::XML(act.to_s).css('//div2')
-  scenes.each do |scene|
-    newscene = newact.scenes.create(number: currScene)
-    currScene = currScene + 1
-    lines = Nokogiri::XML(scene.to_s).css('//sp')
-    stages = Nokogiri::XML(scene.to_s).css('stage').to_a
-    lines.each do |line|
-      speaker = Nokogiri::XML(line.to_s).css('speaker')
-      milestones = Nokogiri::XML(line.to_s).css('milestone')
-      spwords = Nokogiri::XML(line.to_s).css('w','c','pc')
-      milestones.each do |ms|
-        lineNum = ms.attr("n").to_s.split(".")[2]
-        wordIDs = ms.attr("corresp").to_s.split(" ")
-        wordIDs = wordIDs.map { |w| w.gsub("#","")}
-        allthewords = ""
-        wordIDs.each do |id|
-          spwords.each do |word|
-            if word.attr('xml:id').to_s == id
-              allthewords = allthewords + word.inner_text
+  currIndex = 1
+  acts= doc.css('//div1')
+  acts.each do |act|
+    newact = play.acts.create(number: currAct)
+    currAct = currAct + 1
+    currScene = 1
+    scenes = Nokogiri::XML(act.to_s).css('//div2')
+    scenes.each do |scene|
+      newscene = newact.scenes.create(number: currScene)
+      currScene = currScene + 1
+      lines = Nokogiri::XML(scene.to_s).css('//sp')
+      stages = Nokogiri::XML(scene.to_s).css('stage').to_a
+      lines.each do |line|
+        speaker = Nokogiri::XML(line.to_s).css('speaker')
+        milestones = Nokogiri::XML(line.to_s).css('milestone')
+        spwords = Nokogiri::XML(line.to_s).css('w','c','pc')
+        milestones.each do |ms|
+          lineNum = ms.attr("n").to_s.split(".")[2]
+          wordIDs = ms.attr("corresp").to_s.split(" ")
+          wordIDs = wordIDs.map { |w| w.gsub("#","")}
+          allthewords = ""
+          wordIDs.each do |id|
+            spwords.each do |word|
+              if word.attr('xml:id').to_s == id
+                allthewords = allthewords + word.inner_text
+              end
             end
           end
+          newline = newscene.lines.create(number: lineNum, words: allthewords, speaker: speaker.inner_text)
         end
-        newline = newscene.lines.create(number: lineNum, words: allthewords, speaker: speaker.inner_text)
       end
     end
   end
 end
-#scene = act.scenes.create(number: 1)
-#scene.lines.create(number: 1, words: "This is a line")
