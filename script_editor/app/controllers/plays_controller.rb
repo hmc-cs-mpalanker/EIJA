4645cdca46830b3e0b572edee5d8603e9e9221c2
hmc-cs-mpalanker <mpalanker@cs.hmc.edu>
2017-10-15 18:17:49 -0700

    Make database Queries use Joins

--- script_editor/app/controllers/plays_controller.rb
+++ script_editor/app/controllers/plays_controller.rb
@@ -3,6 +3,6 @@ require 'nokogiri'
 class PlaysController < ApplicationController
   def show
     @play = Play.find(params[:id])
-    @acts = Act.where(:play_id => @play.id).order(:number)
+    @acts = Act.joins(:play).where(:play_id => @play.id).order(:number)
   end
 end
