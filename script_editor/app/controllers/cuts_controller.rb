8adcf975170d22a2bad11de6e810852463429ae1
DragonTechno <12izzy3@gmail.com>
2017-11-05 15:08:31 -0800

    Slight Testing Changes
    Added @cut to my cut controller, recreated new.html.erb for testing, messed around with dataCut for testing

--- script_editor/app/controllers/cuts_controller.rb
+++ script_editor/app/controllers/cuts_controller.rb
@@ -1,8 +1,8 @@
 class CutsController < ApplicationController
   def new
-  	Cut.create(edit_id: params[:editI],word_id: params[:wordI])
+  	@cut = Cut.create(edit_id: params[:editI],word_id: params[:wordI])
   end
   def delete
-  	Cut.where(edit_id: params[:editI],word_id: params[:wordI]).first.delete
+  	@cut = Cut.where(edit_id: params[:editI],word_id: params[:wordI]).first.delete
   end
 end
