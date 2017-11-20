899952b7753add7ad6dd2bb12fd2d91cb1d8b12a
hmc-cs-mpalanker <mpalanker@cs.hmc.edu>
2017-11-19 19:56:27 -0800

    removed seeding construct default testing edit

--- script_editor/db/seeds.rb
+++ script_editor/db/seeds.rb
@@ -126,15 +126,3 @@ files.each do |file|
     end
   end
 end
-
-play = Play.find(1)
-edit = play.edits.create()
-# Act.joins(:play).where(:play_id => play.id).order(:number).each do |act|
-#   Scene.joins(:act).where(:act_id => act.id).order(:number).each do |scene|
-#     Line.joins(:scene).where(:scene_id => scene.id).order(:number).each do |line|
-#       Word.joins(:line).where(:line_id => line.id).order(:place).each do |word|
-#         Cut.create(edit_id: edit.id, word_id: word.id)
-#       end
-#     end
-#   end
-# end
