34c7a2283e3128bc2fe99302ed2aaa8badb0c785
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-02-24 17:09:45 -0800

    LineCut DB set up successfully

--- script_editor/app/models/line_cut.rb
+++ script_editor/app/models/line_cut.rb
@@ -1,2 +1,4 @@
 class LineCut < ApplicationRecord
+	belongs_to :edit
+  	belongs_to :word
 end
--- script_editor/app/models/word.rb
+++ script_editor/app/models/word.rb
@@ -1,5 +1,6 @@
 class Word < ApplicationRecord
   belongs_to :line
   has_many :edits, through: :cuts
+  has_many :edits, through: :line_cuts
   has_many :cuts
 end
