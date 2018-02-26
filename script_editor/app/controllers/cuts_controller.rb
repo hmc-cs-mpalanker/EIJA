8c36f8ea731d90b5570e0bc57f27d05415c5339f
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-02-26 15:41:53 -0800

    LineCut insert and delete relationship works

--- script_editor/app/controllers/cuts_controller.rb
+++ script_editor/app/controllers/cuts_controller.rb
@@ -6,22 +6,39 @@ class CutsController < ApplicationController
   	# get the word -> the line.id of the word -> update the length of the line with the update method
   	# this is word_id in the data-base
   	@word = @cut.word
-
   	@line = @word.line
 
-  	editLength = @line.currLength - 1
-  	@line.update(currLength: editLength)
+		if @line.currLength > 0
+			editLength = @line.currLength - 1
+			@line.update(currLength: editLength)
 
-  	# add editId and lineId for LineCut relationship
-  	if editLength == 0
-  		LineCut.create(edit_id: params[:editI], line_id: @line.id)
-  	end
+			# add editId and lineId for LineCut relationship
+			if editLength == 0
+				LineCut.create(edit_id: params[:editI], line_id: @line.id)
+			end
+		end
 
   end
 
   def delete
   	# get the cut data-entry with the appropriate entries
   	#  delete it from the DB
-  	Cut.where(edit_id: params[:editI],word_id: params[:wordI]).first.delete
-  end
+  	@cut = Cut.where(edit_id: params[:editI],word_id: params[:wordI]).first.delete
+
+		# increment the line-length
+		@word = @cut.word
+		@line = @word.line
+
+		# un-cut the line from LineCut table
+		# if currLength is 0
+		if @line.currLength == 0
+			@line.update(currLength: 1)
+			LineCut.where(edit_id: params[:editI], line_id: @line.id).first.delete
+		else
+			editLength = @line.currLength + 1
+			@line.update(currLength: editLength)
+		end
+
+	end
+
 end
