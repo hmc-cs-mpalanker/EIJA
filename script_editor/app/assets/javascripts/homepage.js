52d9086d0398b365c2af74d9a22ac2d9cbb0b5a3
aljohnso <aljohnso@students.pitzer.edu>
2018-03-05 12:47:42 -0800

    updated Front end home page

--- script_editor/app/assets/javascripts/homepage.js
+++ script_editor/app/assets/javascripts/homepage.js
@@ -1,9 +1,13 @@
 // Homepage js
 
-var a = document.getElementById('searchbar');
-a.addEventListener('submit',function(e) {
+var a = $('#searchbar');
+a.submit(function(e) {
     e.preventDefault();
     var b = document.getElementById('searchinput').value;
     document.getElementById("searchquery").innerHTML = b;
     window.location.href = '/plays/' + b;
+});
+
+$(document).on("click", ".flip-container", function () {
+    $(this).toggleClass('hover');
 });
