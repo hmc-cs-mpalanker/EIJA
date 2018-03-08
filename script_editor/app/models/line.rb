0d147298c335e511ea11c18793c4902d83c969d6
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-03-07 21:01:31 -0800

    all_lines :: in-progress

--- script_editor/app/models/line.rb
+++ script_editor/app/models/line.rb
@@ -29,5 +29,55 @@ class Line < ApplicationRecord
     return lines_per_character
   end
 
+  # key all the lines of the play that have not been cut
+  # output: hash where key = [act,scene] pair and value = [lines]
+  #
+
+  # need more work
+  def getLines
+
+    all_lines = Hash.new
+
+    scenes = Scene.all
+
+    arr = []
+    scenes.each_with_index do |item1,index1|
+
+      s = scenes[index1]
+
+      aId = s.act_id
+      sID = s.id
+
+      arr = [aId,sID]
+
+      lines = Line.where(:scene_id => sID)
+
+      lines.each_with_index do |item2, index2|
+        line = lines[index2]
+
+        l_index = line.id
+
+        wds = []
+        if line.currLength != 0
+          words = Word.where(:line_id => l_index)
+
+          words.each_with_index do |item3, index3|
+            wd = words[index3].id
+
+            if Cut.where(:word_id => wd) == 0
+              wds.append(wd)
+            end
+          end
+        end
+      end
+      all_lines[arr => wds]
+  end
+
+    return all_lines
+  end
 
 end
+
+
+# l = Line.new
+# l.getLines
