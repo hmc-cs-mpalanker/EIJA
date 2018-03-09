39b0dfecfe78e7eb64460668bdcd04a0b52ad7ea
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-03-08 19:18:42 -0800

    Data-structure for DB works

--- script_editor/app/models/line.rb
+++ script_editor/app/models/line.rb
@@ -29,89 +29,14 @@ class Line < ApplicationRecord
     return lines_per_character
   end
 
-  # key all the lines of the play that have not been cut
-  # output: hash where key = [act,scene] pair and value = [lines]
-  #
-  # revised output: [[Speaker, lines]]
-  # act -> multiple scenes -> lines -> words
-  # lines -> [[wordId,word]]
-  def getLines
 
-    all_lines = Hash.new
-    # all the scenes in the play
-    scenes = Scene.all
-
-    # key as a tuple
-    arr = []
-    scenes.each_with_index do |item1, index1|
-
-      s = scenes[index1]
-
-      aId = s.act_id
-      sID = s.id
-
-      arr = [aId, sID]
-
-      # all the lines with the same Scene ID
-      lines = Line.where(:scene_id => sID)
-
-      lines_per_scene = []
-      index_lines_per_scene = 0
-
-      # all the lines for a scene
-      lines.each_with_index do |item2, index2|
-
-        # a given line
-        line = lines[index2]
-        l_index = line.id
-
-        wds = []
-        if line.currLength != 0
-          # all the words on a given line
-          words = Word.where(:line_id => l_index)
-
-          words.each_with_index do |item3, index3|
-            wd = words[index3]
-
-            # not in the Cuts table
-            if Cut.where(:word_id => wd.id).length == 0
-              wds.append(wd.text)
-            end
-          end
-          speaker = line.speaker
-          str = wds.join(" ")
-
-          if index_lines_per_scene == 0
-            lines_per_scene.append([speaker, [str]])
-
-          else
-            if lines_per_scene[index_lines_per_scene - 1][0] == speaker
-              lines_per_scene[index_lines_per_scene - 1][1].append(str)
-
-            else
-              lines_per_scene.append([speaker, [str]])
-            end
-            # lines_per_scene.append(wds.join(" "))
-
-            index_lines_per_scene += 1
-
-          end
-        end
-        all_lines[arr] = lines_per_scene
-      end
-
-
-    end
-
-    return all_lines
-  end
-
-
-  # a_line = []
-  # many_lines = [a_line]
-  # a_pair = [spk,many_lines]
-  # all_pairs = [a_pair]
-  def getActScene(act, scene)
+  # input: the scene_id
+  # output: a list of all_pairs
+    # all_pairs = list of 'a_pair'
+    # a_pair = list of 'speaker','many_lines'
+    # many_lines = list of 'a_line'
+    # a_line = list of 'wordID', 'text'
+  def getActScene(scene)
 
     # list of [speaker, many lines]
     all_pairs = []
@@ -142,18 +67,12 @@ class Line < ApplicationRecord
           next
         end
 
-        # many_lines.append(a_line)
-        #
-        # a_pair.append(spk)
-        # a_pair.append(many_lines)
-        # all_pairs.append(a_pair)
 
         if index != 0
           prev_pair = all_pairs[all_pairs.length-1]
 
-
             prev_speaker = prev_pair[0]
-            # puts "#{prev_speaker}"
+
             if prev_speaker == spk
               # to many_lines
               prev_pair[1].append(a_line)
@@ -179,6 +98,25 @@ class Line < ApplicationRecord
     end
 
     return all_pairs
+  end
+
+  # get all the Act-scene pairs ::
+  # output: HashMap, key: [act_id, scene_id], value: [many_lines]
+
+  def getAllActScenes()
+    play = Hash.new
+    scenes = Scene.all
+
+    scenes.each do |scene|
+
+      key = [scene.act_id, scene.id]
+
+      val = getActScene(scene.id)
+
+      play[key] = val
+    end
+
+    return play
   end
 
 
