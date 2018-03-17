1aea00a47c9577d1a61a6644d6ebc2d9e7a4a78d
Alasdair Johnson <aljohnso@students.pitzer.edu>
2018-03-17 12:20:10 -0700

    Fixed modal and got basic layout done

--- script_editor/app/assets/javascripts/edits.js
+++ script_editor/app/assets/javascripts/edits.js
@@ -10,13 +10,33 @@ $(function() {
             $(this).toggleClass('active').siblings().removeClass('active');
         });
     });
+    analytics();
+
+
+
 });
 
 /*
-Above is pro hax
+    Above is pro hax
+    why it works has something to do with confirming that JQuery is loaded before the
+    execution of code
+    however as a web dev I have never seen this before in any other framework
+    another hack that works is to inline script
+    but this is terrible style
  */
 
 function test() {
     console.log($("#synopsis-modal"))
+}
+
+/**
+ * The bellow method binds the Anyalicts element to the analytis modal so when
+ * it is clicked the modal will open
+ */
+function analytics() {
+    $("#play-analytics").click(function () {
+        $("#analytics-modal").modal("show");
+    })
+
 }
 
