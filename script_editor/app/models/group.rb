3ef6dd98466369f3a60e07707f1dbcafaee2f93f
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-03-29 12:04:36 -0700

    added create Group functionality

--- script_editor/app/models/group.rb
+++ script_editor/app/models/group.rb
@@ -1,2 +1,22 @@
 class Group < ApplicationRecord
+
+  # a list of userIds
+  # void: adds users to a new group with a new group Number
+  def createGroup(lst)
+    gNum = -1
+
+    len = Group.all.length
+
+    if len == 0
+      gNum = 1
+    else
+
+      gNum = Group.last.groupNum
+      # the next number for the group
+      gNum +=1
+    end
+    lst.each {|user| Group.create(groupNum: gNum, user_id: user)}
+  end
+
+
 end
