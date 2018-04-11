56704107f3d9b3e045a4104a2c8596cdf9cbbf30
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-04-11 16:24:03 -0700

    added back victor's table -- it works but we have a bug of checking the edits page w/o logging in

--- script_editor/app/controllers/edits_controller.rb
+++ script_editor/app/controllers/edits_controller.rb
@@ -25,6 +25,9 @@ class EditsController < ApplicationController
   end
   def new
     @play = Play.find(params[:id])
+
+    @name = session[:user_id]
+
     @user = current_user
     @edit = Edit.create({:user_id => @user.id, :play_id => @play.id})
     # @link = "/edits/" + (@edit.id.to_s)
