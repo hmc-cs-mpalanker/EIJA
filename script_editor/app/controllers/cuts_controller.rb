a2d994e5c19ab9501af6c34922677a94f5308814
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-04-16 16:25:53 -0700

    BROKEN

--- script_editor/app/controllers/cuts_controller.rb
+++ script_editor/app/controllers/cuts_controller.rb
@@ -4,31 +4,32 @@ require 'json'
 class CutsController < ApplicationController
 
   def new
-		# at runtime can know the current User
-    # userID = current_user.id
-    # puts " THE USER ID IS :: #{userID}"
-		cutAndUncut(params[:payload],params[:meta][:cutOrUncut],1)
+
+    play_id = params[:meta][:playID]
+    curr_id = current_user.id
+
+    new_edit = nil
+
+    if play_id != nil && curr_id != nil
+      arr = Edit.where({user_id: current_user.id , play_id:params[:meta][:playID], groups_id: current_user.groups_id})
+      if arr.length == 0
+        new_edit = EditsController.makeEdit(current_user.id, params[:meta][:playID], current_user.groups_id)
+      else
+        new_edit = Edit.where({user_id: current_user.id , play_id:params[:meta][:playID], groups_id: current_user.groups_id}).update(updated_at: Time.now)
+      end
+    end
+
+    puts "THE EDIT IS : #{new_edit}"
+    puts "THE EDIT ID IS : #{new_edit[0].id}"
+
+    # DO NOT BREAK MY SYSTEM IF NIL
+    if new_edit == nil
+      new_edit = 1
+    end
+
+    cutAndUncut(params[:payload],params[:meta][:cutOrUncut],new_edit[0].id)
+
   end
-
-  def delete
-  	# get the cut data-entry with the appropriate entries
-  	#  delete it from the DB
-  	@cut = Cut.where(edit_id: params[:editI],word_id: params[:wordI]).first.delete
-
-		# increment the line-length
-		@word = @cut.word
-		@line = @word.line
-
-		# un-cut the line from LineCut table
-		# if currLength is 0
-		if @line.currLength == 0
-			@line.update(currLength: 1)
-			LineCut.where(edit_id: params[:editI], line_id: @line.id).first.delete
-		else
-			editLength = @line.currLength + 1
-			@line.update(currLength: editLength)
-		end
-	end
 
   # payload: the list of wordIds to cut/uncut
   # binOpt: true means to cut the word and false means to uncut
@@ -44,12 +45,6 @@ class CutsController < ApplicationController
 
           @cut = Cut.create(edit_id: editId,word_id: wordID)
 
-          # update the edit
-          # need to create the correct editID first
-          # the groupId is the row in the Group table
-          # the groupNum tells the group the user is part of
-          Edit.where(user_id: current_user.id).update(groups_id: current_user.groups_id)
-
           # get the word -> the line.id of the word -> update the length of the line with the update method
           # this is word_id in the data-base
           @word = @cut.word
@@ -63,15 +58,13 @@ class CutsController < ApplicationController
             if editLength == 0
               # the user has an id
               # use that id to get the row in the edits table
-              LineCut.create(edit_id: 1, line_id: @line.id)
+              LineCut.create(edit_id: editId, line_id: @line.id)
             end
           end
         else
           # get the cut data-entry with the appropriate entries
           #  delete it from the DB
-          @cut = Cut.where(edit_id: 1,word_id: wordID).first.delete
-
-          Edit.where(user_id: current_user.id).update(groups_id: current_user.groups_id)
+          @cut = Cut.where(edit_id: editId,word_id: wordID).first.delete
 
           # increment the line-length
           @word = @cut.word
@@ -81,7 +74,7 @@ class CutsController < ApplicationController
           # if currLength is 0
           if @line.currLength == 0
             @line.update(currLength: 1)
-            LineCut.where(edit_id: 1, line_id: @line.id).first.delete
+            LineCut.where(edit_id: editId, line_id: @line.id).first.delete
           else
             editLength = @line.currLength + 1
             @line.update(currLength: editLength)
--- script_editor/app/controllers/edits_controller.rb
+++ script_editor/app/controllers/edits_controller.rb
@@ -3,26 +3,14 @@ require 'open-uri'
 class EditsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    # @acts = Act.joins(:play).where(:play_id => @play.id).order(:number)
-    # @relCuts = Cut.where(:edit_id => @edit.id).pluck(:word_id).to_a.to_set
-    @edit = Edit.find(params[:id])
-    l = Line.new
-    @hash = l.countAnalytics
-    @scene = l.renderActScene(1)
-    # @scene.each do |p|
-    #   puts "#{p}"
-    # end
-    # puts "#{@hash}"
-    # puts "#{@hash.length}"
-    # Line.countAnalytics
-  end
+
   def compress
     @edit = Edit.find(params[:id])
     @play = @edit.play
     @acts = Act.joins(:play).where(:play_id => @play.id).order(:number)
     @relCuts = Cut.where(:edit_id => @edit.id).pluck(:word_id).to_a.to_set
   end
+
   def new
     @play = Play.find(params[:id])
 
@@ -39,6 +27,15 @@ class EditsController < ApplicationController
     @link = "/edits/" + (@edit.id.to_s)
     redirect_to @link
   end
+
+  # make an entry in the Edits table by passing the parameters
+  def self.makeEdit(user_id, play_id, group_id)
+    @play = Play.find(play_id)
+    @edit = Edit.create({:user_id => user_id, :play_id => play_id, :groups_id => group_id})
+    return @edit
+  end
+
+
   # def group_edit
   #   @edit = Edit.find(params[:id])
   #   puts "#{@edit}"
