829a86047acbf9abbe795115b257ad3cbfdfd581
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-04-17 00:37:49 -0700

    Generalized back-end less the matching feature

--- script_editor/app/controllers/analytics_modal_controller.rb
+++ script_editor/app/controllers/analytics_modal_controller.rb
@@ -2,8 +2,11 @@ class AnalyticsModalController < ApplicationController
   def show
     l = Line.new
     @hash = l.countAnalytics(cookies[:play_id])
-    @speakers = l.getAllSpeakers.keys
-    @matrixData = l.charFeatureWrapper
+    @speakers = l.getAllSpeakers(cookies[:play_id]).keys
+
+    # COMMENT OUT FOR NOW
+    # @matrixData = l.charFeatureWrapper
+
     @speakersPretty = @speakers.collect { |x| x.capitalize }
     render :layout => false
   end
