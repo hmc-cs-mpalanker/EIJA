441f546a131deeb77dc15882c9867649f38f6d37
Victor Ginelli <vginelli18@cmc.edu>
2018-04-17 21:51:55 -0700

    commit before merge

--- script_editor/app/assets/javascripts/users.js
+++ script_editor/app/assets/javascripts/users.js
@@ -8,9 +8,9 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+$("#myBtn").click(function(){
+	$("#myModal").modal('show');
+})
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -32,6 +32,5 @@ function showGroupInfo(groupNum) {
             var group =$("#GroupWindow");
             group.html("");//clear previous data
             group.html("abcde");//load new scene
-            detectSelections(); // bind to scence
         };
 }
