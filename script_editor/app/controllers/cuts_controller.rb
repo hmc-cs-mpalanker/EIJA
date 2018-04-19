146053dcd35d5c183595fe56506686a4a431acb7
Dhruv Sawhney <dhruvsawhney96@gmail.com>
2018-04-19 11:32:12 -0700

    Added groupNum logic to cuts/uncut :: before modifying lines structure

--- script_editor/app/controllers/cuts_controller.rb
+++ script_editor/app/controllers/cuts_controller.rb
@@ -47,7 +47,10 @@ class CutsController < ApplicationController
     edit_id = edit_object[0].id
 
     # puts "THE EDIT ID IS: #{edit_id}"
-    cutAndUncut(params[:payload],params[:meta][:cutOrUncut],edit_id)
+    # cutAndUncut(params[:payload],params[:meta][:cutOrUncut],edit_id)
+
+    # the modified function that takes the group_number as an attribute as well
+    cutAndUncut(params[:payload],params[:meta][:cutOrUncut],edit_id,group_number)
 
   end
 
@@ -56,14 +59,62 @@ class CutsController < ApplicationController
   # editId: the editId for the Cut being made :: hardcoded to 1 for now
   #         earlier params[id] was used that was constant for a given user
 
-  def cutAndUncut(payload, binOpt, editId)
+  # def cutAndUncut(payload, binOpt, editId)
+  #
+  #   if payload != nil && payload.length != 0
+  #     for wordID in payload do
+  #
+  #       if binOpt == "true"
+  #
+  #         @cut = Cut.create(edit_id: editId,word_id: wordID)
+  #
+  #         # get the word -> the line.id of the word -> update the length of the line with the update method
+  #         # this is word_id in the data-base
+  #         @word = @cut.word
+  #         @line = @word.line
+  #
+  #         if @line.currLength > 0
+  #           editLength = @line.currLength - 1
+  #           @line.update(currLength: editLength)
+  #
+  #           # add editId and lineId for LineCut relationship
+  #           if editLength == 0
+  #             # the user has an id
+  #             # use that id to get the row in the edits table
+  #             LineCut.create(edit_id: editId, line_id: @line.id)
+  #           end
+  #         end
+  #       else
+  #         # get the cut data-entry with the appropriate entries
+  #         #  delete it from the DB
+  #         @cut = Cut.where(edit_id: editId,word_id: wordID).first.delete
+  #
+  #         # increment the line-length
+  #         @word = @cut.word
+  #         @line = @word.line
+  #
+  #         # un-cut the line from LineCut table
+  #         # if currLength is 0
+  #         if @line.currLength == 0
+  #           @line.update(currLength: 1)
+  #           LineCut.where(edit_id: editId, line_id: @line.id).first.delete
+  #         else
+  #           editLength = @line.currLength + 1
+  #           @line.update(currLength: editLength)
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
+
+  def cutAndUncut(payload, binOpt, editId, group_number)
 
     if payload != nil && payload.length != 0
       for wordID in payload do
 
         if binOpt == "true"
 
-          @cut = Cut.create(edit_id: editId,word_id: wordID)
+          @cut = Cut.create(edit_id: editId,word_id: wordID, groupNum: group_number)
 
           # get the word -> the line.id of the word -> update the length of the line with the update method
           # this is word_id in the data-base
@@ -78,14 +129,17 @@ class CutsController < ApplicationController
             if editLength == 0
               # the user has an id
               # use that id to get the row in the edits table
-              LineCut.create(edit_id: editId, line_id: @line.id)
+              LineCut.create(edit_id: editId, line_id: @line.id, groupNum: group_number)
             end
           end
         else
           # get the cut data-entry with the appropriate entries
           #  delete it from the DB
-          @cut = Cut.where(edit_id: editId,word_id: wordID).first.delete
+          # @cut = Cut.where(edit_id: editId,word_id: wordID).first.delete
 
+          # use the groupNumber instead
+          # at the lines-render, only render for the current group
+          @cut = Cut.where(word_id: wordID, groupNum: group_number).first.delete
           # increment the line-length
           @word = @cut.word
           @line = @word.line
@@ -94,7 +148,8 @@ class CutsController < ApplicationController
           # if currLength is 0
           if @line.currLength == 0
             @line.update(currLength: 1)
-            LineCut.where(edit_id: editId, line_id: @line.id).first.delete
+            # LineCut.where(edit_id: editId, line_id: @line.id).first.delete
+            LineCut.where(line_id: @line.id, groupNum: group_number).first.delete
           else
             editLength = @line.currLength + 1
             @line.update(currLength: editLength)
@@ -103,5 +158,6 @@ class CutsController < ApplicationController
       end
     end
   end
+
 
 end
